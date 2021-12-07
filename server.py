dc7bc502b7128d1885847c4a4ad2fd7fa0f96208
crose1121 <coreycoding1121@outlook.com>
2021-12-06 20:42:36 -0500

    fixes

--- server.py
+++ server.py
@@ -4,19 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',num=3, color='cadetblue')
 
 @app.route('/play')  #This route is never accessed! How do we complete this assignment using only 1 template?
 def play():
-    return render_template('play.html')
+    return render_template('index.html', num=3, color='cadetblue')
 
 @app.route('/play/<num>')
-def num(num):
-    return render_template('play_num.html', num=int(num))
+def num(num = 3):
+    return render_template('index.html', num=int(num), color='cadetblue')
 
 @app.route('/play/<int:num>/<string:color>')
-def num_color(num,color):
-    return render_template('play_num_color.html', num=int(num), color=str(color))
+def num_color(num=3,color='cadetblue'):
+    return render_template('index.html', num=int(num), color=str(color))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+## To only use 1 template, set default values for arguments passed. Default value of 0 to start with nothing.
